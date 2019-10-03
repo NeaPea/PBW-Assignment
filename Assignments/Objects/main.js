699b01d6160021d6e00718077478e22d5cbfc294
linnea.nagelkerke@gmail.com <linnea.nagelkerke@gmail.com>
2019-10-03 10:01:02 +0200

    added new js n html  version

--- Assignments/Objects/main.js
+++ Assignments/Objects/main.js
@@ -1,111 +1,118 @@
-/*"use strict";
-var $ = function (foo) {                            // Save keystrokes
+// JS.OO.0  - Song list
+"use strict";
+var $ = function (foo) {                                         // Save keystrokes
     return document.getElementById(foo);
 }
 
-let submitSong = function (){                              // Function for event listener
+let Song = {                                                // create object
+    init: function(artist, song, year) {                    // Object method                                                        //Constructor - to create an object with different properties(options). 
+        this.artist = artist;                                // initializing properties                                             // THIS specific object
+        this.song = song;                                    
+        this.year = year; 
+
+    },
+    
+    getFullInfo: function() {                                 // another object method
+        return  this.artist + ' ' +  this.song + ', ' + this.year;
+    },
+
+    getHtmlElement: function () {                           // A function that will run each time we click Submit Song              
+        let listItem = document.createElement("LI");
+        let songInfo = document.createTextNode(this.getFullInfo());
+        listItem.appendChild(songInfo);
+        return listItem;
+    }
+}
+
+let submitSong = function () {                               // Function for event listener
     let artistName = $('artist').value;                    // To run when submit is clicked
     let songName = $('song').value;
     let relYear = $('year').value;
 
-    if (artistName.length && songName.length && relYear.length > 0) {
-        $('right').innerHTML = artistName;
-        $('right').innerHTML = songName;
-        $('right').innerHTML = relYear;
+    if (artistName && songName && relYear > 0) {
+        let applySong = Object.create(Song);                    
+        applySong.init(artistName, songName, relYear);
+        $('songList').appendChild(applySong.getHtmlElement());
     } else {
-        console.log('the fuck is this shit?');
+        confirm("You forgot to add an artist, song or release year.");
     }
 }
 
 
-    let Songs = {                                                // create object
-        init: function(artist, song, year, list) {                      // object method
-            this.artist = $('artist').value;                   // initializing properties
-            this.song = $('song').value; 
-            this.year = $('year').value; 
-            this.list = [];
-        },
-        
-        getFullInfo: function() {                                 // another object method
-            return '<p>' + this.artist + ' ' +  this.song + ', ' + this.year + '</p>';
-        }
+
+// JS.OO.1  - Display planet info
+
+let Planet = {                                                      // Create object
+    info: function(dist, sidereal, synodic, eccen, orbital) {       // Object method
+        this.dist = dist;                                           // Initializing the properties
+        this.sidereal = sidereal;
+        this.synodic = synodic;
+        this.eccen = eccen;
+        this.orbital = orbital;
+    },
+
+    getPlanetInfo: function () { 
+        return  "Distance: " + this.dist + "Sidereal Period: " + this.sidereal + "Synodic Period: " + this.synodic + "Eccentricity: " + this.eccen + "Orbital Inclination: " + this.orbital;
+    },
+
+    htmlPrint: function () {                                    // Create the HTML elements for the different planets
+        let txtBox = document.createElement("div");
+        let planetInfo = document.createTextNode(this.getPlanetInfo);
+        txtBox.style.backgroundColor = "lightgrey";
+        txtBox.style.padding = "15px";
+        txtBox.appendChild(planetInfo);
+        return txtBox;
     }
-    
-    
-    let applySong = Object.create(Songs);      // create object based on Person
-    console.log(applySong.getFullInfo());     // invoke method       
+}
 
+let printPlanet = function () {
+    let planets = [];                                             // Create an array with planets
 
+    let mercury = Object.create(Planet);
+    mercury.info("0.3871 AU", "87.97 days", "115.88 days", "0.206", "7.00°");
+    planets["Mercury"] = mercury;
 
+    let venus = Object.create(Planet);
+    venus.info("0.7233 AU", "224.70 d", "583.92 d", "0.007", "3.39°");
+    planets["Venus"] = venus;
+
+    let earth = Object.create(Planet);
+    earth.info("1 AU", "1 year", "365 days", "0.017", "0.00°");
+    planets["Earth"] = earth;
+
+    let mars = Object.create(Planet);
+    mars.info("1.5237 AU", "1.8809 years", "779.94 days", "0.093", "1.85°");
+    planets["Mars"] = mars;
+
+    let jupiter = Object.create(Planet);
+    jupiter.info("5.2028 AU", "11.862 years", "398.9 days", "0.048", "3.39°");
+    planets["Jupiter"] = jupiter;
+
+    let saturn = Object.create(Planet);
+    saturn.info("9.5388 AU", "29.458 years", "378.1 days", "0.056", "2.49°");
+    planets["Saturn"] = saturn;
+
+    let uranus = Object.create(Planet);
+    uranus.info("19.1914 AU", "84.01 years", "369.7 days", "0.046", "0.77°");
+    planets["Uranus"] = uranus;
+
+    let neptune = Object.create(Planet);
+    neptune.info("30.0611 AU", "164.79 years", "367.5 days", "0.010", "1.77°");
+    planets["Neptune"] = neptune;
+
+    let pluto = Object.create(Planet);
+    pluto.info("39.5294 AU", "248.5 years", "366.7 days", "0.248", "17.15°");
+    planets["Pluto"] = pluto;
+
+    let displayPlanet = Object.create(Planet);
+    displayPlanet.info(planets);
+    $("planetInfoTxt").appendChild(displayPlanet.htmlPrint());
+}
 
 
 let windowLoad = function () {
-    $('submitAnswer').addEventListener('click', submitSong)
-}
-
-window.addEventListener('load', windowLoad);*/
-
-"use strict";
-let Songs = {                                                // create object
-    init: function(artist, song, year) {                      // object method
-        this.artist = $('artist').value;                  // initializing properties
-        this.song = $('song').value; 
-        this.year = $('year').value; 
-    },
-        
-    getFullInfo: function() {                                 // another object method
-        return '<p>' + this.artist + ' ' + this.song + ' ' +  this.year + '</p>';   //nml
-    }
-}
-
-var $ = function (foo) {                            // Save keystrokes
-    return document.getElementById(foo);
-}
-
-let submitSong = function (){                              // Function for event listener
-    let artistName = $('artist').value;                    // To run when submit is clicked
-    let songName = $('song').value;
-    let relYear = $('year').value;
-    
-    if (artistName.length && songName.length && relYear.length > 0) {
-
-        let applySong = Object.create(Songs);      // create object based on Songs
-        applySong.init();                          // initialize object nml
-        songs.push(applySong);                     // put object into array nml
-        console.log(applySong.getFullInfo());      // invoke method, debug       
-/*        
-        $('right').innerHTML = artistName;          // throw out
-        $('right').innerHTML += ' ' + songName;
-        $('right').innerHTML += ' ' + relYear;
-*/
-        printArray();                               // print array instead see line 40 nml
-    } else {
-        console.log('the fuck is this shit?');
-    }
-}
-
-const printArray = function() {
-    // clear $('right'), ie either clear innerHTML, or loop throug firstChild and remove    
-    // loop through array and print every object with obj.getFullInfo()
-    // use for of loop
-    let div1 = $('right');
-    while (div1.firstChild) {                       // Removing all children from an element
-            div1.removeChild(div1.firstChild);
-    }
-    for (song of songs) {                           // loop them onto screen
-        let p = song.getFullInfo();
-        $('right').innerHTML = p;
-    }
-
-}
-
-
-
-
-let songs = [];
-
-const windowLoad = function () {
-    $('submitAnswer').addEventListener('click', submitSong)
+    $('submitAnswer').addEventListener('click', submitSong);
+    $('earth').addEventListener('click', printPlanet);
 }
 
 window.addEventListener('load', windowLoad);
